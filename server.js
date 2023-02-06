da18a8a9ce7a1cda603d0dc800424760b033e890
Roman Makhov <nekromantos30888666@gmail.com>
2023-02-06 07:54:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+const inexPage = fs.readFileSync(path.resolve(__dirname, 'dist', 'index.html'));
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static('dist'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, "./src/index.html"));
-});
+app.get('*', (req, res) => res.set('Content-Type', 'text/html').send(inexPage));
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running  http://localhost:${PORT}`);
 });
